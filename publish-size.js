25b0c133802084a73655f56a03aa467adcac61d3
anotherjsguy <kuldeepkeshwar@gmail.com>
2019-07-09 16:05:58 +0530

    size-store api configurable via env variable

--- publish-size.js
+++ publish-size.js
@@ -1,23 +1,23 @@
 const { repo, sha, event, branch, pull_request_number, ci } = require('ci-env');
 const axios = require('axios');
-const url = 'https://size-plugin-store.now.sh';
+const SIZE_STORE_ENDPOINT = process.env.SIZE_STORE_ENDPOINT || 'https://size-plugin-store.now.sh' ;
 async function publishDiff(diff) {
 	if (ci && event == 'pull_request') {
 		try {
 			const params = { ci,repo, branch, sha, pull_request_number, diff };
-			await axios.post(`${url}/diff`, params);
+			await axios.post(`${SIZE_STORE_ENDPOINT}/diff`, params);
 		}
 		catch (error) {
 			console.error('error: while publishing diff', error);
 		}
 	}
 }
-async function publishSizes(diff) {
+async function publishSizes(size) {
 	// TODO: read allowed branch from configuration
 	if (ci && event == 'push' && branch==='master') {
 		try {
-			const params = { ci,repo, branch, sha, pull_request_number, diff };
-			await axios.post(`${url}/sizes`, params);
+			const params = { ci,repo, branch, sha, pull_request_number, size };
+			await axios.post(`${SIZE_STORE_ENDPOINT}/size`, params);
 		}
 		catch (error) {
 			console.error('error: while publishing sizes', error);
